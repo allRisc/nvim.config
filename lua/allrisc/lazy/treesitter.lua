82aeb60288f0ef0d28c17f37267d27f671fba2e0
Benjamin Davis <allrisc.dev@gmail.com>
2024-08-06 07:50:21 -0700

    Added systemverilog to treesitter

--- lua/allrisc/lazy/treesitter.lua
+++ lua/allrisc/lazy/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-            ensure_installed = { "c", "lua", "markdown", "verilog", "python" },
+            ensure_installed = { "c", "lua", "markdown", "verilog", "python", "systemverilog" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
